f6652015c7ced80fd11bd52f09939a0b75ea99be
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-11-19 00:26:29 +0900

    create register

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import "./App.css";
 import Login from "./container/Login/Login";
 import Top from "./container/Top/Top";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./container/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Login />}></Route>
-        <Route path={`/Success`} element={<Top />}></Route>
+        <Route path={`/Top`} element={<Top />}></Route>
+        <Route path={`/Register`} element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
