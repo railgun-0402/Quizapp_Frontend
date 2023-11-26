7ae2aacb91862583fbacc0aa4b49e67f35eabd49
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-11-26 23:28:04 +0900

    connect get user

--- src/container/Top/Top.tsx
+++ src/container/Top/Top.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+const userObject = {
+  id: 1,
+  name: "管理者",
+  email: "admin@test.com",
+  password: "123456",
+  auth: "1",
+};
+
+const data = userObject.id;
 
 const Top = () => {
-  return <div>ログイン成功！</div>;
+  const [categoryList, setCategoryList] = useState<any>();
+
+  const requestGet = async () => {
+    let result;
+    result = await getData();
+
+    console.log(`result = ${JSON.stringify(result)}`);
+    // setCategoryList(result);
+  };
+
+  const getData = async () => {
+    const url =
+      "https://ru20i00bnj.execute-api.ap-northeast-1.amazonaws.com/default/quiz_get_user";
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.error(`error = ${err}`);
+    }
+  };
+  return (
+    <>
+      <h1>ユーザー情報</h1>
+      <button onClick={requestGet}>API Test</button>
+      <hr />
+      {categoryList}
+    </>
+  );
 };
 
 export default Top;
